f6dfd898394819b5a69802d2fe4f2724d8a8ce88
acevif <acevif@gmail.com>
2026-01-27 08:40:22 +0900

    chore: bump nixpkgs to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Rust dev shell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
